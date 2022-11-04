ce188355c63301567a7f6d2f27d4483b7b6dc040
Thomas Aelbrecht <thomasaelbrecht@live.com>
2022-11-04 09:03:13 +0100

    Starter code

---
+++ src/pages.jsx
@@ -0,0 +1,88 @@
+import { LoremIpsum } from 'react-lorem-ipsum';
+
+export const Home = () => (
+  <div>
+    <h1>Welkom!</h1>
+    <p>Hier is nog niet zoveel te zien.</p>
+  </div>
+);
+
+export const About = () => (
+  <div>
+    <h1>Over ons</h1>
+    <LoremIpsum p={2} />
+  </div>
+);
+
+export const Contact = () => (
+  <div>
+    <h1>Contact</h1>
+    <LoremIpsum p={2} />
+  </div>
+);
+
+export const NotFound = () => {
+  return (
+    <div>
+      <h1>Pagina niet gevonden</h1>
+      <p>
+        Er is geen pagina met op deze url, probeer iets anders.
+      </p>
+    </div>
+  );
+};
+
+// const Services = () => (
+//   <div>
+//     <h1>Our services</h1>
+//     <LoremIpsum p={2} />
+//   </div>
+// );
+
+// const History = () => (
+//   <div>
+//     <h1>History</h1>
+//     <LoremIpsum p={2} />
+//   </div>
+// );
+
+// const Location = () => (
+//   <div>
+//     <h1>Location</h1>
+//     <LoremIpsum p={2} />
+//   </div>
+// );
+
+// const products = [{
+//   id: 1,
+//   name: 'Confituur',
+//   price: 2.50
+// }, {
+//   id: 2,
+//   name: 'Choco',
+//   price: 3.50
+// }, {
+//   id: 3,
+//   name: 'Coco-cola',
+//   price: 3.20
+// }, {
+//   id: 4,
+//   name: 'Fanta',
+//   price: 3.00
+// }, {
+//   id: 5,
+//   name: 'Sprite',
+//   price: 2.90
+// }];
+
+// export const Products = () => (
+//   <div>
+//     <ul>
+//       {products.map(({ id, name }) => (
+//         <li key={id}>
+//           {name}
+//         </li>
+//       ))}
+//     </ul>
+//   </div>
+// );
